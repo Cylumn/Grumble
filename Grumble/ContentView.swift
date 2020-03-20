a2621e53642a2f033c81e33f3995d7942f58aef4
Allen Chang <allensylverostchang@gmail.com>
2020-03-20 10:54:18 -0700

    Initial Commit

---
+++ Grumble/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Grumble
+//
+//  Created by Allen Chang on 3/20/20.
+//  Copyright © 2020 Cylumn, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GrumbleTests/GrumbleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GrumbleTests.swift
+//  GrumbleTests
+//
+//  Created by Allen Chang on 3/20/20.
+//  Copyright © 2020 Cylumn, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Grumble
+
+class GrumbleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
